830d56d0d71466c31afb736e59f70af1bbc74e91
Kosta <sergaki1@msu.edu>
2026-05-12 12:35:00 -0400

    wip

--- src/Embedded/ATCC_F/ATCC_F.ino
+++ src/Embedded/ATCC_F/ATCC_F.ino
@@ -12,7 +12,6 @@ FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
 #define CAN1_BAUDRATE 1000000
 
 // signal definitions
-#include "CAN/SR26_CAN1.hpp"
 #include "CAN/SR26_CAN2.hpp"
 
 // sensor definitions
@@ -22,15 +21,10 @@ FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
 #include "can_send.hpp"
 
 // brake bias calculation for front ATCC
-#include "bias_calc.hpp"
+// #include "bias_calc.hpp"
 
 // rotor temp calculation for ATCC
-#include "rotor_temp_calc.hpp"
-// FL rotor is pin 2 on j2 ain71 channel 6 adc 5
-// fr rotor is pin 3 on j2 ain61 channel 2 adc 5
-
-// suspot fr is pin 4 on j2 ain101 channel 7 on adc5
-// suspot fl is pin 5 on j2 ain161 channel 5 on adc 4
+// #include "rotor_temp_calc.hpp"
 
 // rainbow RGB
 #include "rainbow_pixels.hpp"
@@ -40,20 +34,15 @@ FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
 
 const int GLO_NeoPixel_teensy_pin = 0;
       int GLO_NeoPixel_brightness_percent = 10; // 0 - 100 %
-Adafruit_NeoPixel GLO_neopixel(1, GLO_NeoPixel_teensy_pin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel GLO_neopixel(0, GLO_NeoPixel_teensy_pin, NEO_GRB + NEO_KHZ800);
 
-// ATCC Module Select - 0 front, 1 back
-const int ATCCMS = 0;
-
-FreqMeasureMulti freqFR;
+FreqMeasureMulti freqRR;
 FreqMeasureMulti freqRL;
+float sumRR = 0, sumRL = 0;
+int countRR = 0, countRL = 0;
 
-#define freq_pinFR 2 // ain2 pin25 on J2 FR
+#define freq_pinRR 2 // ain2 pin25 on J2 RR
 #define freq_pinRL 3 // ain3 pin17 on J1 RL
-
-float sumFR=0, sumRL=0;
-int countFR=0, countRL=0;
-elapsedMillis timeout;
 
 void setup() {
 
@@ -65,7 +54,7 @@ void setup() {
 
   // Initialize serial communication
   Serial.begin(112500);
-  Serial.println("starting");
+  // Serial.println("starting");
   
   //initialize SPI communication
   SPI.begin();
@@ -77,55 +66,41 @@ void setup() {
   //initialize ADCs
   initialize_ADCs();
 
-  digitalWrite(32, HIGH);
+  // freqRR.begin(freq_pinRR);
+  // freqRL.begin(freq_pinRL);
 
   GLO_neopixel.setPixelColor(0, 0, 255, 0); // green
   GLO_neopixel.show();
 
-  while (!Serial) ; // wait for Arduino Serial Monitor
-  delay(10);
-  Serial.println("FreqMeasureMulti Begin");
-  delay(10);
-  freqFR.begin(freq_pinFR);
-  freqRL.begin(freq_pinRL);
+  // while (!Serial) ; // wait for Arduino Serial Monitor
+  // delay(10);
+  // Serial.println("FreqMeasureMulti Begin");
+  // delay(10);
   
-  if (freqFR.available()) {
-    sumFR = sumFR + freqFR.read();
-    countFR = countFR + 1;
-  }
-  if (freqRL.available()) {
-    sumRL = sumRL + freqRL.read();
-    countRL = countRL + 1;
-  }
-  // print results every half second
-  if (timeout > 500) {
-    if (countFR > 0) {
-      ATCCF_wheelSpeedFR = freqFR.countToFrequency(sumFR / countFR);
-    } else {
-      Serial.print("(no pulses)");
-    }
-    Serial.print(",  ");
-    if (countRL > 0) {
-      ATCCF_wheelSpeedFL = freqRL.countToFrequency(sumRL / countRL);
-    } else {
-      Serial.print("(no pulses)");
-    }
-    Serial.println();
-    sumFR = 0;
-    sumRL = 0;
-    countFR = 0;
-    countRL = 0;
-    timeout = 0;
-  }
 }
 
 void loop() {
 
-  //msu_pixels(GLO_neopixel);
-
+  // rainbow_pixels(GLO_neopixel);
+  
+  // if (freqRR.available()) {
+  //   sumRR = sumRR + freqRR.read();
+  //   countRR = countRR + 1;
+  //   Serial.println("countRR: ");
+  //   Serial.println(countRR);
+  // }
+  
+  // if (freqRL.available()) {
+  //   sumRL = sumRL + freqRL.read();
+  //   countRL = countRL + 1;
+  // }
+  
   sample_ADCs();
-  log_test_sens();
-  //readWheelSpeed();
+  
+  // static EasyTimer wheelTimer(100); // 100 Hz
+  // if (wheelTimer.isup()){
+  //   readWheelSpeed();
+  // }
 
-  //send_can1();
+  send_can1();
 }
