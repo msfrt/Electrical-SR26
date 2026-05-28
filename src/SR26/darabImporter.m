cc618fe96e30ce2312a68629e4c0ab5805856fda
Kosta <sergaki1@msu.edu>
2026-05-28 13:11:36 -0400

    cache cleanup

--- src/SR26/darabImporter.m
+++ src/SR26/darabImporter.m
@@ -1,9 +1,12 @@
 % Load CSV
-data = readtable('pow.csv');
+data = readtable('20251206_curr.csv');
 
 % Convert to numeric arrays
 time = data.xtime_s_;  % [s]
-rpm = data.PM_motorSpeed_rpm_;
-t = data.PM_torqueFeedback_Nm_;
+c = data.PM_dcBusCurrent_A_;
+ct = data.PM_commandedTorque_Nm_;
+volt = data.PM_dcBusVolt_V_;
+spd = data.PM_motorSpeed_rpm_;
+ft = data.PM_torqueFeedback_Nm_;
 
-imu_input = timeseries([rpm, t], time);
+current_input2 = timeseries([c, ct, volt, spd, ft], time);
